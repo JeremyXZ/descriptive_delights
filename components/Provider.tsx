061f9ec8f3ab7272b4a26fbcd77f271d8f1a7348
JeremyXZ <jeremyuk168@gmail.com>
2023-06-04 18:13:07 +0100

    added db/database.ts and db credentials in env file

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -1,12 +1,10 @@
-"use client"
-import { SessionProvider } from 'next-auth/react'
+"use client";
+import { SessionProvider } from "next-auth/react";
 
-const Provider = ({ children, session}) => {
-  return (
-    <SessionProvider session={session}  >
-      {children}
-    </SessionProvider>
-  )
-}
+// like context api's context provider, to provide session data and manage session state
+//will be used in app/layout.tsx to provide global data
+const Provider = ({ children, session }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
 
-export default Provider
+export default Provider;
